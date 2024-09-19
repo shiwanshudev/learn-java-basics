cc34a160aee09a2fa8424936a55bf3f68742a8db
Shiwanshu Shubham <Shiwanshu Shubham>
2024-09-19 14:59:31 +0530

    Learn loops and input output

---
+++ t2_grade/Grade.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Grade{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        char continueChoice;
+        do{
+            System.out.print("Enter your score(0-100): ");
+            int score = scanner.nextInt();
+            char grade;
+
+            if(score >=90)
+                grade = 'A';
+            else if(score >= 80)
+                grade = 'B';
+            else if (score >= 70)
+                grade = 'C';
+            else if (score >= 60)
+                grade = 'D';
+            else 
+                grade = 'F';
+            System.out.println("Your score is: "+ grade);
+            System.out.print("Do you want to calculate another grade?: (y/n)");
+            continueChoice = scanner.next().charAt(0);
+            continueChoice = Character.toLowerCase(continueChoice);
+            }while(continueChoice == 'y');
+        }
+    }
